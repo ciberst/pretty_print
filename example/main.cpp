cefa24df437500f65edbccd783dc3c5c9b6ca71f
ciberst <ciberst@gmail.com>
2019-09-12 21:40:19 +0300

    fix code style add pretty_print for variant

--- example/main.cpp
+++ example/main.cpp
@@ -5,6 +5,7 @@
 #include <unordered_map>
 #include <variant>
 #include <vector>
+
 #include "pretty_print/pretty_print.hpp"
 
 int main() {
@@ -15,15 +16,25 @@ int main() {
 	std::map<int, int> map = {{1, 2}, {2, 3}, {3, 4}};
 	auto pair = std::make_pair("123"s, 12);
 	std::unordered_map<std::string, std::map<std::string, std::optional<int>>> mapmap = {
-	    {"test"s, {{"1"s, 2}, {"2"s, 3}, {"3"s, 4}}},
-	    {"hello"s, {{"1"s, 2}, {"2"s, 3}, {"3"s, 4}}},
-	    {"world"s, {{"1"s, 2}, {"2"s, 3}, {"3"s, {}}}}};
+        {"test"s, {{"1"s, 2}, {"2"s, 3}, {"3"s, 4}}},
+        {"hello"s, {{"1"s, 2}, {"2"s, 3}, {"3"s, 4}}},
+        {"world"s, {{"1"s, 2}, {"2"s, 3}, {"3"s, {}}}}};
 	int c_arr[] = {1, 2, 3, 4, 5, 6};
 	auto tuple = std::make_tuple("1", 2, 3, 4.5);
 	std::forward_list<std::string> words1{"the", "frogurt", "is", "also", "cursed"};
 	std::optional<std::string> opt{"string"s};
 	std::optional<std::string> null_opt;
+    std::variant<int, std::string> variant;
+    variant = "123";
 
+    std::vector<std::variant<int, std::string, std::pair<double, int>>> vector_varints = {
+        std::variant<int, std::string, std::pair<double, int>>{1},
+        std::variant<int, std::string, std::pair<double, int>>{"string"},
+        std::variant<int, std::string, std::pair<double, int>>{std::pair{2.5, 1}},
+        std::variant<int, std::string, std::pair<double, int>>{}};
+
+    pretty_print(std::cout, vector_varints) << std::endl;
+    pretty_print(std::cout, variant) << std::endl;
 	pretty_print(std::cout, pair) << std::endl;
 	pretty_print(std::cout, a) << std::endl;
 	pretty_print(std::cout, "hello") << std::endl;
@@ -38,15 +49,16 @@ int main() {
 	std::cout << "=====================================" << std::endl;
 
 	std::cout << pretty_print(pair) << std::endl
-	          << pretty_print(a) << std::endl
-	          << pretty_print("hello") << std::endl
-	          << pretty_print(map) << std::endl
-	          << pretty_print(mapmap) << std::endl
-	          << pretty_print(c_arr) << std::endl
-	          << pretty_print(tuple) << std::endl
-	          << pretty_print(words1) << std::endl
-	          << pretty_print(opt) << std::endl
-	          << pretty_print(null_opt) << std::endl;
+              << pretty_print(a) << std::endl
+              << pretty_print("hello") << std::endl
+              << pretty_print(map) << std::endl
+              << pretty_print(mapmap) << std::endl
+              << pretty_print(c_arr) << std::endl
+              << pretty_print(tuple) << std::endl
+              << pretty_print(words1) << std::endl
+              << pretty_print(opt) << std::endl
+              << pretty_print(null_opt) << std::endl;
+
 
 	return 0;
 }
